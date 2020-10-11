871890eab5c83a8c569286c3b191a8ca30fb24b9
premchandAllu <premchandallu1999@gmail.com>
2020-10-11 21:54:46 +0530

    Added: Method for finding maximum of three

---
+++ main/java/com/blz/FindMaximum/UserMaximum.java
@@ -0,0 +1,11 @@
+package com.blz.FindMaximum;
+
+public class UserMaximum {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Finding Maximum of three Variables");
+
+	}
+
+}
---
+++ test/java/com/blz/FindMaximum/TestMaximum.java
@@ -0,0 +1,5 @@
+package com.blz.FindMaximum;
+
+public class TestMaximum {
+
+}
