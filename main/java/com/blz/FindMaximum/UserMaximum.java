b0551f4346b70823d1f4af444c6ef31c765080a2
premchandAllu <premchandallu1999@gmail.com>
2020-10-11 22:59:21 +0530

    Refactor: Method to find maximum by one generic

--- main/java/com/blz/FindMaximum/UserMaximum.java
+++ main/java/com/blz/FindMaximum/UserMaximum.java
@@ -1,34 +1,46 @@
 package com.blz.FindMaximum;
-
 import java.util.Scanner;
 
 public class UserMaximum {
 
 	public static Scanner sc = new Scanner(System.in);
-	
-	public static String findMaximum(String x, String y, String z) {
-		String max=x;
-		if(x.compareTo(y)>0 && x.compareTo(z)>0) {
-			max=x;
-			System.out.println(x+" is the maximum String");
-		}else if(z.compareTo(y)>0 && z.compareTo(x)>0) {
-			System.out.println(z+" is the maximum String");
-			max=z;
+
+
+	public static <E extends Comparable<E>> E findMaximum(E x, E y, E z) {
+		E max = x;
+		if (x.compareTo(y) > 0 && x.compareTo(z) > 0) {
+			max = x;
+			System.out.println(x + " is the maximum ");
+		} else if (z.compareTo(y) > 0 && z.compareTo(x) > 0) {
+			System.out.println(z + " is the maximum ");
+			max = z;
 		} else {
-			System.out.println(y+" is the maximum String");
-			max=y;
+			System.out.println(y + " is the maximum ");
+			max = y;
 		}
 		return max;
 	}
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        System.out.println("Welcome to Test maximum Code");
-        
-        System.out.println("Enter three Strings");
-        String a=sc.next();
-        String b=sc.next();
-        String c=sc.next();
-        System.out.println("Maximum string is "+findMaximum(a,b,c));
+		System.out.println("Welcome to Test maximum Code");
+
+		System.out.println("Enter three integer variables");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		System.out.println("Maximum number is " + findMaximum(a, b, c));
+
+		System.out.println("Enter three float variables");
+		float x = sc.nextFloat();
+		float y = sc.nextFloat();
+		float z = sc.nextFloat();
+		System.out.println("Maximum number is " + findMaximum(x, y, z));
+
+		System.out.println("Enter three Strings");
+		String str1 = sc.next();
+		String str2 = sc.next();
+		String str3 = sc.next();
+		System.out.println("Maximum string is " + findMaximum(str1, str2, str3));
 	}
 }
--- test/java/com/blz/FindMaximum/TestMaximum.java
+++ test/java/com/blz/FindMaximum/TestMaximum.java
@@ -7,7 +7,73 @@ import org.junit.Test;
 public class TestMaximum {
 
 	@Test
-	public void testMaximumAtFirstPosition() {
+	public void testIntegerMaximumAtFirstPosition() {
+		Integer maximum=UserMaximum.findMaximum(81, 45, 18);
+		boolean b;
+		if(maximum.equals(81))
+			b=true;
+		else
+			b=false;
+		Assert.assertEquals(true, b);
+	}
+	
+	@Test
+	public void testIntegerMaximumAtSecondPosition() {
+		Integer maximum=UserMaximum.findMaximum(18, 81, 45);
+		boolean b;
+		if(maximum.equals(81))
+			b=true;
+		else
+			b=false;
+		Assert.assertEquals(true, b);
+	}
+	
+	@Test
+	public void testIntegerMaximumAtThirdPosition() {
+		Integer maximum=UserMaximum.findMaximum(18, 45, 81);
+		boolean b;
+		if(maximum.equals(81))
+			b=true;
+		else
+			b=false;
+		Assert.assertEquals(true, b);
+	}
+	
+		@Test
+		public void testFloatMaximumAtFirstPosition() {
+			Float maximum=UserMaximum.findMaximum(81.1f, 45.5f, 18.8f);
+			boolean b;
+			if(maximum.equals(81.1f))
+				b=true;
+			else
+				b=false;
+			Assert.assertEquals(true, b);
+		}
+		
+		@Test
+		public void testFloatMaximumAtSecondPosition() {
+			Float maximum=UserMaximum.findMaximum(18.8f, 81.1f, 45.5f);
+			boolean b;
+			if(maximum.equals(81.1f))
+				b=true;
+			else
+				b=false;
+			Assert.assertEquals(true, b);
+		}
+		
+		@Test
+		public void testFloatMaximumAtThirdPosition() {
+			Float maximum=UserMaximum.findMaximum(18.8f, 45.5f, 81.1f);
+			boolean b;
+			if(maximum.equals(81.1f))
+				b=true;
+			else
+				b=false;
+			Assert.assertEquals(true, b);
+		}
+	
+	@Test
+	public void testStringMaximumAtFirstPosition() {
 		String maximum = UserMaximum.findMaximum("Peach","Banana", "Apple");
 		boolean b;
 		if (maximum.equals("Peach"))
@@ -18,7 +84,7 @@ public class TestMaximum {
 	}
 
 	@Test
-	public void testMaximumAtSecondPosition() {
+	public void testStringMaximumAtSecondPosition() {
 		String maximum = UserMaximum.findMaximum("Apple", "Peach", "Banana");
 		boolean b;
 		if (maximum.equals("Peach"))
@@ -29,7 +95,7 @@ public class TestMaximum {
 	}
 
 	@Test
-	public void testMaximumAtThirdPosition() {
+	public void testStringMaximumAtThirdPosition() {
 		String maximum = UserMaximum.findMaximum("Apple", "Banana", "Peach");
 		boolean b;
 		if (maximum.equals("Peach"))
