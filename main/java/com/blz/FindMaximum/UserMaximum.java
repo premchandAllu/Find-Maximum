9cfb2d172e7e71e6bd56a663ec53821c03e261d4
premchandAllu <premchandallu1999@gmail.com>
2020-10-11 22:21:20 +0530

    Added: Method for maximum of three numbers

--- main/java/com/blz/FindMaximum/UserMaximum.java
+++ main/java/com/blz/FindMaximum/UserMaximum.java
@@ -1,11 +1,35 @@
 package com.blz.FindMaximum;
 
+import java.util.*;
+
 public class UserMaximum {
 
-	public static void main(String[] args) {
-		
-		System.out.println("Finding Maximum of three Variables");
+	public static Scanner sc = new Scanner(System.in);
 
+	public static Integer findMaximum(Integer x, Integer y, Integer z) {
+		int max = 0;
+		if (x.compareTo(y) > 0 && x.compareTo(z) > 0) {
+			max = x;
+			System.out.println(x + " is the maximum number");
+		} else if (z.compareTo(y) > 0 && z.compareTo(x) > 0) {
+			System.out.println(z + " is the maximum number");
+			max = z;
+		} else {
+			System.out.println(y + " is the maximum number");
+			max = y;
+		}
+		return max;
 	}
 
+	public static void main(String[] args) {
+
+		System.out.println("Welcome to Test maximum Code");
+
+		System.out.println("Enter three variables");
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		
+		findMaximum(a, b, c);
+	}
 }
--- test/java/com/blz/FindMaximum/TestMaximum.java
+++ test/java/com/blz/FindMaximum/TestMaximum.java
@@ -1,5 +1,40 @@
 package com.blz.FindMaximum;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class TestMaximum {
 
+	@Test
+	public void testMaximumAtFirstPosition() {
+		Integer maximum=UserMaximum.findMaximum(81,45,18);
+		boolean b;
+		if(maximum.equals(23))
+			b=true;
+		else
+			b=false;
+		Assert.assertEquals(true, b);
+	}
+	
+	@Test
+	public void testMaximumAtSecondPosition() {
+		Integer maximum=UserMaximum.findMaximum(18,81,45 );
+		boolean b;
+		if(maximum.equals(20))
+			b=true;
+		else
+			b=false;
+		Assert.assertEquals(true, b);
+	}
+	
+	@Test
+	public void testMaximumAtThirdPosition() {
+		Integer maximum=UserMaximum.findMaximum(18, 45, 81);
+		boolean b;
+		if(maximum.equals(40))
+			b=true;
+		else
+			b=false;
+		Assert.assertEquals(true, b);
+	}
 }
